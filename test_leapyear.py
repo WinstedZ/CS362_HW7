f5b7d6809bac2ec6475ba47dcecaa29ae3ac4328
Zakary Winsted <43679286+WinstedZ@users.noreply.github.com>
2021-03-07 12:40:04 -0800

    created test for the functionality of 100 not being a leap year

--- test_leapyear.py
+++ test_leapyear.py
@@ -3,7 +3,10 @@ import unittest
 
 class TestCase(unittest.TestCase):
     def testfourhundred(self):
-        self.assertEqual(leapyear.fourhundred(400),"Is a leap year")
+        self.assertEqual(leapyear.leap(400),"Is a leap year")
     
+    def testonehundred(self):
+        self.assertEqual(leapyear.leap(100),"Is not a leap year")
+        
 if __name__ == '__main__':
     unittest.main(verbosity=2)
